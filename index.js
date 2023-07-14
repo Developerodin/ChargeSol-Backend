5b743424866ef2e35666ac16f112d84dd8055962
Akshaypareek01 <akshay96102@gmail.com>
2023-07-14 15:09:04 +0530

    working on admin access

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {cors: {
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
