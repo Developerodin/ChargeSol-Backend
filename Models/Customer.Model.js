d06791d0ae7a74e7dcf57b3d3a33b77b4c2a5431
Akshaypareek01 <akshay96102@gmail.com>
2023-10-16 11:32:48 +0530

    working

--- Models/Customer.Model.js
+++ Models/Customer.Model.js
@@ -11,7 +11,6 @@ const CustomerSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, 'Please provide your email'],
         unique: true,
         validate: [validator.isEmail, 'Please provide a valid email']
     },
